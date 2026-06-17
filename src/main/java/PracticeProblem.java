874bbc8681cf2bbd1d81bd76ae84cbe057c867f0
VisaaganG <343505582@tdsb.ca>
2026-06-17 03:04:28 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int[] createIntArray() {
+		int[] num = new int[5];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = i + 1;
+		}
+		return num;
 	}
+	
+	public static String[] createArray(String first, String second, String third, String fourth) {
+		String[] words = new String[4];
+		words[0] = first;
+		words[1] = second;
+		words[2] = third;
+		words[3] = fourth;
+		return words;
+	}
+
+	public static String getElement(int n, String[] arr) {
+		return arr[n];
+	}
+
+	public static void replaceElement(int n, double value, double[] arr) {
+		arr[n] = value;
+	}
+
+
 
 }
